bb803fa87dabdb89446536dff0a63cc3ed68e913
mbelen <belen.alegre@gmail.com>
2014-06-10 15:03:59 -0300

    Entidad area de trabajo

---
+++ src/Backend/AdminBundle/Entity/AreaTrabajo.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace Backend\AdminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * AreaTrabajo
+ */
+class AreaTrabajo
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     */
+    private $responsable;
+
+    /**
+     * @var string
+     */
+    private $observaciones;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $modifiedAt;
+
+    /**
+     * @var boolean
+     */
+    private $habilitada;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return AreaTrabajo
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set responsable
+     *
+     * @param string $responsable
+     * @return AreaTrabajo
+     */
+    public function setResponsable($responsable)
+    {
+        $this->responsable = $responsable;
+    
+        return $this;
+    }
+
+    /**
+     * Get responsable
+     *
+     * @return string 
+     */
+    public function getResponsable()
+    {
+        return $this->responsable;
+    }
+
+    /**
+     * Set observaciones
+     *
+     * @param string $observaciones
+     * @return AreaTrabajo
+     */
+    public function setObservaciones($observaciones)
+    {
+        $this->observaciones = $observaciones;
+    
+        return $this;
+    }
+
+    /**
+     * Get observaciones
+     *
+     * @return string 
+     */
+    public function getObservaciones()
+    {
+        return $this->observaciones;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return AreaTrabajo
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set modifiedAt
+     *
+     * @param \DateTime $modifiedAt
+     * @return AreaTrabajo
+     */
+    public function setModifiedAt($modifiedAt)
+    {
+        $this->modifiedAt = $modifiedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get modifiedAt
+     *
+     * @return \DateTime 
+     */
+    public function getModifiedAt()
+    {
+        return $this->modifiedAt;
+    }
+
+    /**
+     * Set habilitada
+     *
+     * @param boolean $habilitada
+     * @return AreaTrabajo
+     */
+    public function setHabilitada($habilitada)
+    {
+        $this->habilitada = $habilitada;
+    
+        return $this;
+    }
+
+    /**
+     * Get habilitada
+     *
+     * @return boolean 
+     */
+    public function getHabilitada()
+    {
+        return $this->habilitada;
+    }
+}
