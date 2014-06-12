3a7800a14617aa5934ee15eb81a241ac5e4b619d
mbelen <belen.alegre@gmail.com>
2014-06-11 23:34:38 -0300

    sucursal

---
+++ src/Backend/AdminBundle/Form/SucursalType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Backend\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ClienteType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('responsable')
+            ->add('calle')
+            ->add('numero')
+            ->add('piso')
+            ->add('cp')
+            ->add('telefono')
+            ->add('fax')
+            ->add('email')
+            
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Backend\AdminBundle\Entity\Sucursal'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'backend_adminbundle_sucursal';
+    }
+}
